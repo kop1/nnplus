58011f94d640f4bc43d3313a41fe633142297d9f
bb <bb@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2011-02-24 17:04:29 +0000

    rar file list
    git-svn-id: svn://svn.newznab.com/nn/branches/nnplus@1379 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/pages/ajax_rarfilelist.php
+++ www/pages/ajax_rarfilelist.php
@@ -1,6 +1,21 @@
 <?php
+require_once(WWW_DIR."/lib/releasefiles.php");
 
 if (!$users->isLoggedIn())
 	$page->show403();
 
-print "meepers";
+if (!isset($_REQUEST["id"]))
+	$page->show404();
+
+$rf = new ReleaseFiles();
+$files = $rf->getByGuid($_REQUEST["id"]);
+
+if (count($files) == 0)
+	print "No files";
+else
+{
+	print "<ul>\n";
+	foreach ($files as $f)
+		print "<li>".htmlentities($f["name"], ENT_QUOTES)."</li>\n";
+	print "</ul>";
+}
