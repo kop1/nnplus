41c0f7875850f21e9b7ecce95979ba57d90ac72c
bb <bb@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2011-03-02 14:16:52 +0000

    dont show mediainfo if not supplied
    git-svn-id: svn://svn.newznab.com/nn/branches/nnplus@1450 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/pages/ajax_mediainfo.php
+++ www/pages/ajax_mediainfo.php
@@ -16,19 +16,33 @@ else
 {
 	print "<h3 class=\"tooltiphead\">extended media info...</h3>\n";
 	print "<table>\n";
-	print "<tr><th>Container Format:</th><td>".htmlentities($redata["containerformat"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Bitrate:</th><td>".htmlentities($redata["overallbitrate"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Duration:</th><td>".htmlentities($redata["videoduration"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Format:</th><td>".htmlentities($redata["videoformat"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Codec:</th><td>".htmlentities($redata["videocodec"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Dimension:</th><td>".htmlentities($redata["videowidth"], ENT_QUOTES)."x".htmlentities($redata["videoheight"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Aspect:</th><td>".htmlentities($redata["videoaspect"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Framerate:</th><td>".htmlentities($redata["videoframerate"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Audio Format:</th><td>".htmlentities($redata["audioformat"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Mode:</th><td>".htmlentities($redata["audiomode"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Bitrate Mode:</th><td>".htmlentities($redata["audiobitratemode"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Bitrate:</th><td>".htmlentities($redata["audiobitrate"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Channels:</th><td>".htmlentities($redata["audiochannels"], ENT_QUOTES)."</td></tr>\n";
-	print "<tr><th>Sample Rate:</th><td>".htmlentities($redata["audiosamplerate"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["containerformat"] != "")
+		print "<tr><th>Container Format:</th><td>".htmlentities($redata["containerformat"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["overallbitrate"] != "")
+		print "<tr><th>Bitrate:</th><td>".htmlentities($redata["overallbitrate"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["videoduration"] != "")
+		print "<tr><th>Duration:</th><td>".htmlentities($redata["videoduration"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["videoformat"] != "")
+		print "<tr><th>Format:</th><td>".htmlentities($redata["videoformat"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["videocodec"] != "")
+		print "<tr><th>Codec:</th><td>".htmlentities($redata["videocodec"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["videowidth"] != "")
+		print "<tr><th>Dimension:</th><td>".htmlentities($redata["videowidth"], ENT_QUOTES)."x".htmlentities($redata["videoheight"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["videoaspect"] != "")
+		print "<tr><th>Aspect:</th><td>".htmlentities($redata["videoaspect"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["videoframerate"] != "")
+		print "<tr><th>Framerate:</th><td>".htmlentities($redata["videoframerate"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["audioformat"] != "")
+		print "<tr><th>Audio Format:</th><td>".htmlentities($redata["audioformat"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["audiomode"] != "")
+		print "<tr><th>Mode:</th><td>".htmlentities($redata["audiomode"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["audiobitratemode"] != "")
+		print "<tr><th>Bitrate Mode:</th><td>".htmlentities($redata["audiobitratemode"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["audiobitrate"] != "")
+		print "<tr><th>Bitrate:</th><td>".htmlentities($redata["audiobitrate"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["audiochannels"] != "")
+		print "<tr><th>Channels:</th><td>".htmlentities($redata["audiochannels"], ENT_QUOTES)."</td></tr>\n";
+	if ($redata["audiosamplerate"] != "")
+		print "<tr><th>Sample Rate:</th><td>".htmlentities($redata["audiosamplerate"], ENT_QUOTES)."</td></tr>\n";
 	print "</table>";
 }	
