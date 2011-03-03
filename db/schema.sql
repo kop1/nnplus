72c9b0fdf15971c8e535729e7f30c74ec932ce8d
bb <bb@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2011-03-03 22:07:20 +0000

    user series db schema
    git-svn-id: svn://svn.newznab.com/nn/branches/nnplus@1474 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- db/schema.sql
+++ db/schema.sql
@@ -566,6 +566,17 @@ CREATE TABLE `users` (
   PRIMARY KEY  (`ID`)
 ) ENGINE=MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `userseries`;
+CREATE TABLE `userseries` (
+  `ID` INT(16) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `userID` int(16) NOT NULL,
+  `rageID` int(16) NOT NULL,
+  `createddate` DATETIME NOT NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE INDEX ix_userseries_userID ON userseries (userID, `rageID`);
+
 DROP TABLE IF EXISTS `userroles`;
 CREATE TABLE `userroles` (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
