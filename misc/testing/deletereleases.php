b3dc054ad891bd6a79cbbe658f87a27998dfabbf
bb <bb@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2011-04-13 09:56:16 +0000

    additional delete function to remove spam with only setup.exe inside the rars.
    git-svn-id: svn://svn.newznab.com/nn/branches/nnplus@1592 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- misc/testing/deletereleases.php
+++ misc/testing/deletereleases.php
@@ -11,8 +11,17 @@ require_once(FS_ROOT."/../../www/lib/releases.php");
 $releases = new Releases();
 $db = new Db;
 
-$rel = $db->query("select * from releases where totalpart = 1 and groupID in (select ID from groups where name = 'alt.binaries.cd.image') ");
+//
+// delete all releases for a group which only has x number of files
+//
+//$sql = "select * from releases where totalpart = 1 and groupID in (select ID from groups where name = 'alt.binaries.cd.image')";
 
+//
+// delete all releases where the only file inside the rars is setup.exe
+//
+$sql = "select releaseID from ( select releasefiles.releaseID, name, count(*) filenum, totnum from releasefiles left outer join (   select releaseID, count(*) as totnum from releasefiles group by releaseID ) x on x.releaseID = releasefiles.releaseID where releasefiles.name = 'setup.exe' group by releasefiles.releaseID, name ) y where y.filenum = y.totnum"; 
+
+$rel = $db->query($sql);
 echo "about to delete ".count($rel)." release(s)";
 
 foreach ($rel as $r) 
